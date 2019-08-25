d5734a10622547849453544cc9303f149e36a884
Ikhyeon <kih199212@gmail.com>
2019-08-25 17:42:45 +0900

    Add onClick event

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -4,12 +4,47 @@ import './index.scss';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+function SingleItem(props) {
+    console.log(props.itemName[props.order])
+    return (
+        <div className="recipe__box--item">
+           {props.itemName[props.order] && <img src={`/img/${props.itemName[props.order]}.png`}></img>} 
+        </div>
+    )
+}
 
+function CombinedItem() {
+    return <div className="recipe__box--item recipe__box--combined"></div>
+}
+
+class RecipeBox extends React.Component {
+
+    render(){
+        //console.log(this.props.selectedItem)
+        return (
+            <div className="recipe__box">
+                <SingleItem 
+                    key={0}
+                    order={0}
+                    itemName={this.props.selectedItem}
+                />
+                <i className="material-icons">add</i>
+                <SingleItem
+                    key={1}
+                    order={1}
+                    itemName={this.props.selectedItem}
+                />
+                <i className="material-icons">arrow_forward</i>
+                <CombinedItem/>
+            </div>
+        )
+    }
+}
 
 
 function RecipePicture(props) {
     return(
-        <img className="recipe__items--round" src={`/img/${props.itemName}.png`}></img>
+        <img className="recipe__items--round" src={`/img/${props.itemName}.png`} onClick={() => {props.onClick(props.itemName)}}></img>
     )
 }
 
@@ -20,26 +55,21 @@ class ItemRecipes extends React.Component {
             <RecipePicture
                 key={itemName}
                 itemName={itemName}
+                onClick={ this.props.onClick}
             />
         )
     }
     
     render(){
-        const itemArray = [
-            'B.F._Sword',
-            'Recurve_Bow',
-            'Chain_Vest',
-            'Negatron_Cloak',
-            'Needlessly_Large_Rod',
-            'Tear_of_the_Goddess',
-            'Giant\'s_Belt',
-            'Spatula'
-        ]
         const itemPictures = []
-        itemArray.forEach( (item) => {
+        this.props.itemList.forEach( (item) => {
             itemPictures.push(this.renderImg(item))
         } )
-        return itemPictures
+        return (
+            <div className="recipe__items">
+                {itemPictures}
+            </div>
+        )
     }
 }
 
@@ -47,16 +77,59 @@ class ItemRecipes extends React.Component {
 class TeamFightTactical extends React.Component {
     constructor(props){
         super(props)
+        this.state = {
+            selectedItem: Array(2).fill(null),
+            currentItem: '',
+            itemList: [
+                'B.F._Sword',
+                'Recurve_Bow',
+                'Chain_Vest',
+                'Negatron_Cloak',
+                'Needlessly_Large_Rod',
+                'Tear_of_the_Goddess',
+                'Giant\'s_Belt',
+                'Spatula'
+            ]
+        }
+        this.handleClick = this.handleClick.bind(this)
     }
 
+    handleClick(itemName){
+        // From Child to Parent
+        const selected = this.state.selectedItem
 
-    render(){
+        if(selected[0] === null){
+            selected[0] = itemName
+        }else if(selected[1] === null){
+            selected[1] = itemName
+        }else{
+            return
+        }
+
+        this.setState( () => {
+            return {
+                selectedItem: selected,
+                currentItem: itemName
+            }
+        })
+        
+    }
+
+    componentDidUpdate(prevProps, prevState){
+
+    }
+
+    render(){   
+        
         return (
             <div className="recipe">
-                <div className="recipe__box"></div>
-                <div className="recipe__items">
-                    <ItemRecipes/>
-                </div>
+                <RecipeBox 
+                    selectedItem={this.state.selectedItem}
+                />
+                <ItemRecipes
+                    itemList={this.state.itemList}
+                    onClick={this.handleClick}
+                />
             </div>
         )
     }
